d602247f2c4d8898905a13fd7d07a27f20e09aff
Adrien BRICCHI <adrienbricchi@users.noreply.github.com>
2021-01-24 00:09:52 +0100

    Tab scroll

--- app/src/main/java/org/adrienbricchi/waitingformoranis/models/Show.java
+++ app/src/main/java/org/adrienbricchi/waitingformoranis/models/Show.java
@@ -23,7 +23,6 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
