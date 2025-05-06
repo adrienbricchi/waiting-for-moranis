5dee150998f31453a63d51c73b5f11a1f356d54f
Adrien Bricchi <adrien.bricchi@libriciel.coop>
2025-05-06 11:28:21 +0200

    Fix the copyright

--- app/src/main/java/org/adrienbricchi/waitingformoranis/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/org/adrienbricchi/waitingformoranis/ui/main/SectionsPagerAdapter.java
@@ -1,11 +1,10 @@
 /*
  * Waiting For Moranis
- * Copyright (C) 2020-2024
+ * Copyright (C) 2020-2025
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,6 +13,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 package org.adrienbricchi.waitingformoranis.ui.main;
