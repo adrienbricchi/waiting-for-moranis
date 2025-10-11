69823a09a7f59ba7098c994e8d8c194a74bb9d6c
Adrien <adrien.bricchi@libriciel.coop>
2025-10-11 12:56:17 +0200

    Upgrade Junit

--- app/src/test/java/org/adrienbricchi/waitingformoranis/models/ReleaseTest.java
+++ app/src/test/java/org/adrienbricchi/waitingformoranis/models/ReleaseTest.java
@@ -19,11 +19,11 @@
 
 package org.adrienbricchi.waitingformoranis.models;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ReleaseTest {
